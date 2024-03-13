6bfecf0e125f4ad8a567150e3c950bf570b601b4
Kajol shah <106387777+Kajolshah29@users.noreply.github.com>
2024-03-13 10:58:22 +0530

    Create hollowrectangle.java

---
+++ hollowrectangle.java
@@ -0,0 +1,30 @@
+class hollowrectangle {
+	static void print_rectangle(int rows, int cols)
+	{
+		for (int i = 1; i <= rows; i++)
+		{
+			for (int j = 1; j <= cols; j++)
+			{
+				if (i == 1 || i == rows || 
+					j == 1 || j == cols)		 
+					System.out.print("*");		 
+				else
+					System.out.print(" ");		 
+			}
+			System.out.println();
+		}
+	
+	}
+	public static void main(String args[])
+	{
+		print_rectangle(6, 4);
+	}
+}
+----------------------------------------------------
+----------------------OUTPUT------------------------
+****
+*  *
+*  *
+*  *
+*  *
+****
